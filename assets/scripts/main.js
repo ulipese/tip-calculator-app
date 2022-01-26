15daf8f20762362fcbde8f0c39733949f2f8590b
Felipe Sousa <felipe.sousa4030@gmail.com.br>
2022-01-26 13:03:27 -0300

    JS Finished!

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,24 +1,71 @@
+'use strict' // strict mode
+
+const bill = document.querySelector('.bill-input')
 const percentButtons = document.querySelectorAll('.button')
+const customPercent = document.querySelector('.custom-input')
+const numberPeople = document.querySelector('.people-input')
+const tipAmount = document.querySelector('.tip-amount')
+const totalAmount = document.querySelector('.total-amount')
+
+let tip = 1 // global tip
+
+// Valid and Format Bill values
+function validBill(bill) {
+  if (bill.value <= 0) {
+    alert('The bill value must be more than 0')
+    bill.value = 1
+  } else {
+    function formatBill(unformattedBill) {
+      const formattedBill = Number(unformattedBill.replace(',', '.'))
+      return formattedBill
+    }
+    return formatBill(bill.value)
+  }
+}
+
+//Valid and format/style Percent buttons and Custom values
+function validPercents(percent) {
+  percentButtons.forEach(btn => {
+    btn.classList.remove('active')
+    if (btn.value === percent.value) {
+      btn.classList.add('active')
+      tip = btn.value
+      tip = Number(tip.toString().replace('%', ''))
+    }
+  })
+}
+
+// Valid People values
+function validPeople(people) {
+  if (Number(people.value) <= 0) {
+    alert('The people number must to be more than 0')
+    people.value = 1
+  }
+  return Number(people.value)
+}
+
+// Currency Formatter
+const formatter = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD',
+  maximumFractionDigits: 2
+})
+
+// Show tip amount
+const calculateTipAmount = () => {
+  const result = ((validBill(bill) / 100) * tip) / validPeople(numberPeople)
+  return result
+}
+
+// Show total amount
+const calculateTotalAmount = () => {
+  const result =
+    validBill(bill) / validPeople(numberPeople) + calculateTipAmount()
+  return result
+}
+
+// Update the values ​​on the screen
 setInterval(() => {
-  const bill = Number(document.querySelector('.bill-input').value)
-  const validBill = (bill) => {
-
-  }
-  const validPercents = (button) => {
-    percentButtons.forEach(btn => {
-      btn.classList.remove('active');
-    })
-
-  }
-  const validNumberOfPeople = (num) => {
-    
-  }
-  
-  getElement = (element) => {
-    validPercents();
-    element.classList.add('active');
-    
-    console.log(element.value)
-  }
-  
-}, 1000)
+  tipAmount.textContent = formatter.format(calculateTipAmount())
+  totalAmount.textContent = formatter.format(calculateTotalAmount())
+}, 200)
