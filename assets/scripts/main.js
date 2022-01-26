e48dc6afe20adb5aec6cc54280327eb236cdd3c5
Felipe Sousa <felipe.sousa4030@gmail.com.br>
2022-01-26 19:27:05 -0300

    JS bugs fixed

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -78,10 +78,11 @@ const clearAll = () => {
 }
 // Update the values ​​on the screen
 setInterval(() => {
-  if (validBill(bill) <= 0 || validPeople(numberPeople) <= 0 || tip <= 0) {
+  if (validBill(bill) <= 0 || validBill(bill) == undefined || validBill(bill) > 0 && tip == 0 || validPeople(numberPeople) <= 0 || validPeople(numberPeople) == undefined) {
     tipAmount.textContent = formatter.format(0)
     totalAmount.textContent = formatter.format(0)
-  } else {
+  } 
+  else {
     tipAmount.textContent = formatter.format(calculateTipAmount())
     totalAmount.textContent = formatter.format(calculateTotalAmount())
   }
